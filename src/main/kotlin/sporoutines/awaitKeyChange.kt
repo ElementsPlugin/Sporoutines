03d870d3296e70b5ed572a0a72648b6e3f09024e
xDotDash <xdotdash@gmail.com>
2018-12-31 11:43:03 -0600

    Try a different method of waiting key change.

--- src/main/kotlin/sporoutines/awaitKeyChange.kt
+++ src/main/kotlin/sporoutines/awaitKeyChange.kt
@@ -1,42 +1,26 @@
 package sporoutines
 
-import com.google.common.collect.HashMultimap
-import com.google.common.collect.Multimaps
+import org.spongepowered.api.Sponge
 import org.spongepowered.api.data.key.Key
 import org.spongepowered.api.data.value.BaseValue
 import org.spongepowered.api.entity.living.player.Player
-import org.spongepowered.api.event.Listener
+import org.spongepowered.api.event.EventListener
 import org.spongepowered.api.event.data.ChangeDataHolderEvent
-import org.spongepowered.api.event.filter.cause.First
-import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
-object KeyChangeListener {
-
-    data class KeyContinuation<out T>(val expected: T, val continuation: Continuation<Unit>)
-
-    internal val keyMap = Multimaps.synchronizedSetMultimap(
-        HashMultimap.create<Key<out BaseValue<out Any>>, KeyContinuation<Any>>()
-    )
-
-    @Listener
-    fun onValueChange(event: ChangeDataHolderEvent.ValueChange, @First player: Player) {
-        for (value in event.endResult.successfulData) {
-            for (cont in keyMap[value.key]) {
-                if (cont.expected == value.get()) {
-                    cont.continuation.resume(Unit)
-                    keyMap.remove(value.key, cont)
+suspend fun <T> Player.awaitKeyChange(plugin: Any, key: Key<out BaseValue<T>>, expected: T) =
+    suspendCoroutine<Unit> { cont ->
+        val listener = object : EventListener<ChangeDataHolderEvent.ValueChange> {
+            override fun handle(event: ChangeDataHolderEvent.ValueChange) {
+                for (value in event.endResult.successfulData) {
+                    if (value.key == key && value.get() == expected) {
+                        Sponge.getEventManager().unregisterListeners(this)
+                        cont.resume(Unit)
+                    }
                 }
             }
         }
-    }
-}
 
-@Suppress("UNCHECKED_CAST")
-suspend fun <T> Player.awaitKeyChange(key: Key<out BaseValue<T>>, expected: T) = suspendCoroutine<Unit> {
-    KeyChangeListener.keyMap.put(
-        key,
-        KeyChangeListener.KeyContinuation(expected, it) as KeyChangeListener.KeyContinuation<Any>
-    )
-}
+        Sponge.getEventManager().registerListener(plugin, ChangeDataHolderEvent.ValueChange::class.java, listener)
+    }
